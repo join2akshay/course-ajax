91d84b2278498a72a4e59b86f0f1106a62054f4c
tvibhu12 <tvibhu12@gmail.com>
2019-05-05 14:18:55 +0530

    add unplash's API and display image

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -1,12 +1,38 @@
 (function () {
     const form = document.querySelector('#search-form');
     const searchField = document.querySelector('#search-keyword');
-    let searchedForText;
+  // let searchedForText;
     const responseContainer = document.querySelector('#response-container');
 
     form.addEventListener('submit', function (e) {
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+
+
+        const unsplashRequest = new XMLHttpRequest();
+    
+        unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+        unsplashRequest.onload = addImage;
+        unsplashRequest.setRequestHeader('Authorization', 'Client-ID 7a7e82bf707c1d4b987e67b45fb7674abd134b163f6180491bcf1cd05946f1f2');
+        unsplashRequest.send();
+        
+        function addImage(){
+            let htmlContent = '';
+      const data = JSON.parse(this.responseText);
+      const firstImage = data.results[0];
+    
+      htmlContent = `<figure>
+        <img src="${firstImage.urls.regular}" alt="${searchedForText}">
+        <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+      </figure>`;
+    
+      responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+    
+        }
+
     });
+
+    
+  
 })();
